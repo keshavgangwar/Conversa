2956ad256d60309707632c4d99d0a96d6cb72e20
Rohit Kumar <147933301+MeRohit-Kumar@users.noreply.github.com>
2024-10-03 14:11:04 +0530

    Update WSServer.js

---
+++ my-app/WSServer.js
@@ -0,0 +1,59 @@
+const ws = require("ws");
+const jwt = require("jsonwebtoken");
+const fs = require("fs");
+const Message = require("./models/messageModel");
+const { clear } = require("console");
+const { User } = require("./models/userModel");
+
+const createWebSocketServer = (server) => {
+  const wss = new ws.WebSocketServer({ server });
+
+  wss.on("connection", (connection, req) => {
+    const notifyAboutOnlinePeople = async () => {
+      const onlineUsers = await Promise.all(
+        Array.from(wss.clients).map(async (client) => {
+          const { userId, username } = client;
+          const user = await User.findById(userId);
+          const avatarLink = user ? user.avatarLink : null;
+          return{
+            userId,
+            username,
+            avatarLink,
+          };
+        })
+         [...wss.clients].forEach((client) => {
+    client.send(
+      JSON.stringify({
+        online: onlineUsers,
+      })
+    );
+  });
+};
+
+connection.isAlive = true;
+
+connection.timer = setInterval(() => {
+  connection.ping();
+
+  connection.deathTimer = setTimeout(() => {
+    connection.isAlive  = false;
+    clearInterval(connection.timer);  
+    connection.terminate();
+notifyAboutOnlinePeople();
+console.log("dead");
+}, 1000);
+}, 5000);
+
+connection.on("pong", () => {
+  clearTimeout(connection.deathTimer);
+});
+
+const cookies = req.headers.cookie;
+
+if (cookies) {
+  const tokenString   
+ = cookies
+    .split(";")
+    .find((str) => str.startsWith("authToken="));
+
+  if (tokenString) {
