3cb180c7fd6a16fda40606868d9b0b78171042a0
Rohit Kumar <147933301+MeRohit-Kumar@users.noreply.github.com>
2024-10-03 11:00:08 +0530

    db.js

---
+++ my-app/db/db.js
@@ -0,0 +1,9 @@
+module.exports = async () => {
+  try {
+    await mongoose.connect(process.env.DB);
+    console.log("DB CONNECTED SUCCESSFULLY");
+  } catch (error) {
+    console.log(error);
+    console.log("COULD NOT CONNECT TO DB");
+  }
+};
