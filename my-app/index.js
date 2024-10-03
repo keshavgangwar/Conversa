7d56f10c91c50f5aa726a1acbe0c5b069a57e79f
Rohit Kumar <147933301+MeRohit-Kumar@users.noreply.github.com>
2024-10-03 10:55:42 +0530

    Updated index.js

---
+++ my-app/index.js
@@ -0,0 +1,53 @@
+require("dotenvconfig").config();
+const express = require ("express");
+const cors = require("cors");
+const app = express();
+const connection = require("./db/db.js");
+const userRoute = require("./routes/user Route.js");
+const avatarRoute = require("./routes/user Route.js");
+const cookieParser = require('cookie-parser');
+const createWebSocketServer = require("./WsServer.js");
+const path = require("path");
+
+//database connection
+connection();
+app.use(express.json())
+app.use(cookieParser())
+//middlewares
+app.use(express.json());
+const allowedorigins = [
+"http://localhost:5173",
+"http://localhost:4000",
+"https://swifty-chatty-appy.onrender.com"
+];
+const corsoptions = {
+origin: (origin, callback) => {
+if (allowedOrigins. includes (origin) || !origin) { 
+  callback(null, true);
+  }
+else {
+callback(new Error("Not allowed by CORS"));
+     }
+},
+methods: "GET, HEAD, PUT, PATCH, POST, DELETE",
+optionsSuccessStatus: 204,
+credentials: true, // Allow credentials like cookies
+};
+app.use(cors (corsoptions));
+app.use("/api/user", user Route);
+app.use("/api/avatar", avatarRoute);
+const port = process.env.PORT || 8000;
+const server =
+app.listen(port, () => console.log( `Application is Running `);
+createWebSocketServer(server);
+app.use(express.static(path.join(__dirname, "..", "frontend", "dist")));
+
+// app.get('/*', (req, res) => {
+//   res.sendFile(path.join(__dirname, '../frontend/dist/index.html'), (err) => {
+//     if (err) {
+//       console.error('Error sending file:', err);
+//     }
+//   });
+// });
+
+
