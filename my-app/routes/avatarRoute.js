b32eb1ec4c73afec042b9ab42db435360328abaf
Rohit Kumar <147933301+MeRohit-Kumar@users.noreply.github.com>
2024-10-03 11:03:13 +0530

    deployment of the project

---
+++ my-app/routes/avatarRoute.js
@@ -0,0 +1,9 @@
+// avatarRoute.js
+const express = require('express');
+const avatarController = require('../controllers/avatarController');
+const router = express.Router();
+
+router.post("/", avatarController.avatarController);
+router.get("/all", avatarController.getAllAvatars);
+
+module.exports = router;
