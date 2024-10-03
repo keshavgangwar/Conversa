eb784d1ef0cf6a8ac54e150af8ec5e3f44126996
Rohit Kumar <147933301+MeRohit-Kumar@users.noreply.github.com>
2024-10-03 13:57:21 +0530

    Updated emailVerificationController.js

--- my-app/controllers/emailVerificationController.js
+++ my-app/controllers/emailVerificationController.js
@@ -1,1 +1,50 @@
+// emailVerificationController.js
 
+const { User } = require("../models/userModel.js");
+const { Token } = require("../models/tokenModel.js");
+
+const verifyEmail = async (req, res) => {
+  // emailVerificationController.js
+
+const { User } = require("../models/userModel.js");
+const { Token } = require("../models/tokenModel.js");
+
+const verifyEmail = async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+
+    if (!user) {
+      return res.status(400).send({ message: "User doesn't exist" });
+    }
+
+    if (user.verified) {
+      return res.status(400).send({ message: "Email already verified" });
+    }
+
+    // Find the token for the user
+    const token = await Token.findOne({
+      userId: user._id,
+      token: req.params.token,
+    });
+
+ if (!token) {
+      return res.status(400).send({ message: "Invalid Link" });
+    }
+
+    if (token.expiresAt < Date.now()) {
+      user.verificationLinkSent = false;
+      await user.save();
+      return res.status(400).send({ message: "Verification link expired" });
+    }
+
+    user.verified = true;
+    await user.save();
+
+    res.status(200).send({ message: "Email Verified Successfully" });
+  } catch (error) {
+    console.error("Error in verifyEmail:", error);
+    res.status(500).send({ message: "Internal Server Error" });
+  }
+};
+
+  module.exports = verifyEmail;
