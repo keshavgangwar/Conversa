21088dcde07781e1ddbfd8505910fdbd5a7f9b04
Rohit Kumar <147933301+MeRohit-Kumar@users.noreply.github.com>
2024-10-13 23:57:49 +0530

    deployment of the project

---
+++ my-app/controllers/messageController.js
@@ -0,0 +1,20 @@
+const protect = require("../middleware/protect");
+const Message = require("../models/messageModel");
+
+const messageController = async (req, res) => {
+    const { userId } = req.params;
+    const userData = await protect(req);
+    console.log("userData", userData);
+
+    const ourUserId = userData._id;
+    console.log("ourUserId", ourUserId);
+    console.log("userId", userId);
+
+    const messages = await Message.find({
+        sender: { $in: [userId, ourUserId] },
+        recipient: { $in: [userId, ourUserId] },
+    }).sort({ createdAt: 1 });
+
+    res.json(messages);
+  console.log ("messages",messages);
+};
