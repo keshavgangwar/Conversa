97695da98d46e7fd53d967e9fde0537b2ae1622b
Rohit Kumar <147933301+MeRohit-Kumar@users.noreply.github.com>
2024-10-13 23:44:41 +0530

    Deployment of the project

---
+++ my-app/controllers/peopleController.js
@@ -0,0 +1,11 @@
+//peopleController.js
+const { User } = require("../models/userModel");
+
+const peopleController = async (req, res) => {
+  const users = await User.find({ verified: true });
+
+  res.json(users);
+  // console.log(users);
+};
+
+module.exports = peopleController;
