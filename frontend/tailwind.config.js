d52dc012938d90d5b73e502f48410e17ca6c473d
Keshav Gangwar <gangwarkeshav3@gmail.com>
2024-10-15 10:51:58 +0530

    Changes made into tailwind.config

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        dark: "#131313",
+        background: "#202329",
+        primary: "#3f4654",
+        primarySecond: "#6B8AFD",
+        secondary: "#30E0A1",
+        tertiary: "#EB7050",
+      },
+    },
+  },
+  plugins: [],
+};
