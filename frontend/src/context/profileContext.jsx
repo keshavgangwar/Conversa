12dd35e22c523ed6c79d99f99ac3404e8b0c0f6d
Rohit Kumar <147933301+MeRohit-Kumar@users.noreply.github.com>
2024-10-15 12:07:57 +0530

    Update profileContext.jsx

---
+++ frontend/src/context/profileContext.jsx
@@ -0,0 +1,34 @@
+import React, { createContext, useContext, useEffect, useState } from "react";
+import { useAuth } from "./authContext";
+import axios from "axios";
+
+// Create Profile Context
+const ProfileContext = createContext();
+
+// Profile Provider Component
+export const ProfileProvider = ({ children }) => {
+    const { isAuthenticated } = useAuth();
+    const [userDetails, setUserDetails] = useState(null);
+    useEffect(() => {
+      const fetchUserDetails = async () => {
+        try {
+          const response = await axios.get("/api/user/profile");
+          // console.log(response.data);
+          setUserDetails(response.data);
+        } catch (error) {
+          console.log("Error fetching user details in profile", error);
+        }
+      };
+      fetchUserDetails();
+    }, [isAuthenticated]);
+    
+    return (
+      <ProfileContext.Provider value={{ isAuthenticated, userDetails }}>
+        {children}
+      </ProfileContext.Provider>
+    );
+    };
+    
+    export const useProfile = () => {
+      return useContext(ProfileContext);
+    };
