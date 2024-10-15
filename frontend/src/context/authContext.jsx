fe9cb049a53e6c2b26c719f9edb456143ad12e22
Rohit Kumar <147933301+MeRohit-Kumar@users.noreply.github.com>
2024-10-15 12:02:32 +0530

    Update authContext.jsx

---
+++ frontend/src/context/authContext.jsx
@@ -0,0 +1,46 @@
+import Cookies from "js-cookie";
+import React, { createContext, useContext, useState } from "react";
+
+// Create a context
+const AuthContext = createContext();
+
+// AuthProvider component
+const AuthProvider = ({ children }) => {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  const setAuthenticated = (value) => {
+    setIsAuthenticated(value);
+  };
+
+  const checkAuth = () => {
+    const token = Cookies.get("authToken");
+    console.log("Checking authentication...");
+    if (token) {
+
+      console.log("Token exists. Setting authenticated to true.");
+      setAuthenticated(true);
+      console.log(isAuthenticated);
+    } else {
+      console.log("Token does not exist. Setting authenticated to false.");
+      setAuthenticated(false);
+    }
+  };
+
+  const logout = () => {
+    console.log("Logging out...");
+    setAuthenticated(false);
+    Cookies.remove("authToken");  // Optionally remove the token if you want to log out
+  };
+
+  return (
+    <AuthContext.Provider value={{ isAuthenticated, setAuthenticated, checkAuth, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+// Custom hook to use AuthContext
+export const useAuth = () => {
+  return useContext(AuthContext);
+};
+
