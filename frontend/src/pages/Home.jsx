98fa742724ae5a375501457b4cc8046687bfb29f
Paras kapoor <147471931+paraskapoor014@users.noreply.github.com>
2024-10-15 12:59:04 +0530

    Update Home.jsx

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,23 @@
+import Hero from "../components/Hero";
+import { useAuth } from "../context/authContext";
+import LandingNav from "../components/LandingNav";
+import Footer from "../components/Footer";
+import Features from "../components/Features";
+import Payments from "../components/Payments";
+import CustomerLogos from "../components/CustomerLogos";
+
+const Home = () => {
+  const { isAuthenticated } = useAuth();
+  return (
+    <div className="bg-dark">
+        <LandingNav/>
+        <Hero/>
+        <Features/>
+        <Payments/>
+        <CustomerLogos/>
+        <Footer/>
+    </div>
+    );
+    };
+  
+    export default Home;
