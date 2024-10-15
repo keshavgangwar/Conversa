3908cb1f766ae10b2f0f3a8002f25fea45258332
Paras kapoor <147471931+paraskapoor014@users.noreply.github.com>
2024-10-15 12:59:56 +0530

    Update ChatMessages.jsx

---
+++ frontend/src/components/Chat/ChatMessages.jsx
@@ -0,0 +1,60 @@
+// ChatMessages.jsx
+import React, { useEffect, useRef, useState } from "react";
+
+const ChatMessages = ({ messages, userDetails, selectedUserId }) => {
+  const messagesContainerRef = useRef(null);
+  // console.log(messages);
+
+  useEffect(() => {
+    const container = messagesContainerRef.current;
+    if (container) {
+      // Set the container scrollTop to the scrollHeight with smooth behavior
+      container.scrollTo({
+        top: container.scrollHeight,
+        behavior: "smooth",
+      });
+    }
+  }, [messages,messagesContainerRef]);
+  return (
+    <div
+      className="absolute bottom-24 w-full px-7 lg:px-20 left-1"
+      ref={messagesContainerRef}
+    >
+      {!!selectedUserId && (
+        <div className="flex flex-col gap-2">
+          {messages.map((message) => (
+            <div
+              key={message._id}
+              className={`text-white ${
+                message.sender !== userDetails._id
+                  ? 'bg-primary self-start rounded-r-2xl'
+                  : 'bg-primarySecond self-end rounded-l-2xl'
+              } relative group rounded-b-2xl px-5 py-3`}
+            >
+              <div
+                style={{ wordWrap: 'break-word' }}
+                className="flex flex-wrap max-w-[500px] overflow-hidden"
+              >
+                {message.text}
+              </div>
+              <div
+                className={`absolute top-0 w-0 h-0 ${
+                  message.sender !== userDetails._id
+                    ? 'border-r-primary-left-4 border-r-[20px]'
+                    : 'rounded-l-lg -right-4 border-l-primary-left-4 border-l-[20px]'
+                } border-b-[20px] border-b-transparent`}
+                ></div>
+                </div>
+          ))}
+          </div>
+      )}
+      {selectedUserId && !messages.length &&(
+        <div className="text-gray-500 flex items-end justify-center">
+        Start a conversation
+      </div>
+    )}
+  </div>
+);
+};
+
+export default ChatMessages;
