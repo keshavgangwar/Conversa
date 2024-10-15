bbef66f5122844e241ee8b45866ef07787be4801
me-sunny <120056191+me-sunny@users.noreply.github.com>
2024-10-15 14:10:46 +0530

    Update CustomerLogos.jsx

---
+++ frontend/src/components/CustomerLogos.jsx
@@ -0,0 +1,134 @@
+import React from "react";
+
+const CustomerLogos = () => {
+    return (
+        <section className="min-h-[30vh]">
+            <div className="py-8 lg:py-16 mx-auto max-w-screen-xl px-4 lg:px-6 xl:px-8">
+                <div className="grid grid-cols-2 gap-8 sm:gap-12 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-4">
+                    
+                    {/* First Logo */}
+                    <a href="#" className="flex justify-center items-center">
+                        <svg
+                            className="h-9 hover:text-white"
+                            viewBox="0 0 125 35"
+                            fill="currentColor"
+                            xmlns="http://www.w3.org/2000/svg"
+                        >
+                            <path
+                                fillRule="evenodd"
+                                clipRule="evenodd"
+                                d="M64.828 7.11521C64.828 8.52061 63.6775 9.652..."
+                                fill="currentColor"
+                            />
+                        </svg>
+                    </a>
+
+                    {/* Second Logo */}
+                    <a href="#" className="flex justify-center items-center">
+                        <svg
+                            className="h-9 hover:text-white"
+                            viewBox="0 0 86 29"
+                            fill="currentColor"
+                            xmlns="http://www.w3.org/2000/svg"
+                        >
+                            <path
+                                fillRule="evenodd"
+                                clipRule="evenodd"
+                                d="M11.6008 10.2627V13.2312L18.6907 13.2281C18..."
+                                fill="currentColor"
+                            />
+                        </svg>
+                    </a>
+
+                    {/* Third Logo */}
+                    <a href="#" className="flex justify-center items-center">
+                        <svg
+                            className="h-8 hover:text-white"
+                            viewBox="0 0 151 34"
+                            fill="currentColor"
+                            xmlns="http://www.w3.org/2000/svg"
+                        >
+                            <g clipPath="url(#clip0_3753_27919)">
+                                <path
+                                    d="M150.059 16.1144V13.4753H146.783V9.37378L1..."
+                                    fill="currentColor"
+                                />
+                            </g>
+                            <defs>
+                                <clipPath id="clip0_3753_27919">
+                                    <rect
+                                        width="150"
+                                        height="32.8125"
+                                        fill="white"
+                                        transform="translate(0.0820312 0.835449)"
+                                    />
+                                </clipPath>
+                            </defs>
+                        </svg>
+                    </a>
+
+                    {/* Fourth Logo */}
+                    <a href="#" className="flex justify-center items-center">
+                        <svg
+                            className="h-9 hover:text-white"
+                            viewBox="0 0 124 38"
+                            fill="currentColor"
+                            xmlns="http://www.w3.org/2000/svg"
+                        >
+                            <path
+                                d="M50.8299 17.3952C54.7246 18.342 56.3124 19.8..."
+                                fill="currentColor"
+                            />
+                        </svg>
+                    </a>
+
+                    {/* Fifth Logo */}
+                    <a href="#" className="flex justify-center items-center">
+                        <svg
+                            className="h-9 hover:text-white"
+                            viewBox="0 0 137 37"
+                            fill="currentColor"
+                            xmlns="http://www.w3.org/2000/svg"
+                        >
+                            <path
+                                d="M53.3228 13.9636C51.5883 13.9636 50.7303 15...."
+                                fill="currentColor"
+                            />
+                            <path
+                                d="M120.249 13.9636C118.514 13.9636 117.656 15...."
+                                fill="currentColor"
+                            />
+                            <path
+                                d="M62.7138 22.5371C61.7709 22.7549 61.2821 22..."
+                                fill="currentColor"
+                            />
+                            <path
+                                d="M74.9258 25.0783H78.8688V10.9255H74.9258V25..."
+                                fill="currentColor"
+                            />
+                            <path
+                                d="M83.2111 19.6471C83.2111 18.6705 84.1184 17..."
+                                fill="currentcolor"
+                            />
+                            <path
+                                d="M97.373 13.9662C95.0905 13.9662 94.2223 16.1..."
+                                fill="currentcolor"
+                            />
+                            <path
+                                d="M102.258 14.2285V25.0782H106.201V14.2285C106..."
+                                fill="currentColor"
+                            />
+                            <path
+                                d="M104.218 10.8157C102.885 10.8157 101.805 11..."
+                                fill="currentColor"
+                            />
+                        </svg>
+                    </a>
+
+                </div>
+            </div>
+        </section>
+    );
+};
+
+export default CustomerLogos;
