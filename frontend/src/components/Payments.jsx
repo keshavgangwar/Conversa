a4d98024fca107da07d987c82e2d32e47f27ec81
Rohit Kumar <147933301+MeRohit-Kumar@users.noreply.github.com>
2024-10-15 12:00:28 +0530

    Update Payments.jsx

---
+++ frontend/src/components/Payments.jsx
@@ -0,0 +1,227 @@
+import React from "react";
+
+const Payments = () => {
+  return (
+    <section className="">
+      <div className="py-8 px-4 mx-auto max-w-screen-xl lg:py-10">
+        <div className="mx-auto max-w-screen-md text-center mb-16">
+          <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-gray-900">
+            Designed for business teams like yours
+          </h2>
+          <p className="mb-5 font-light sm:text-xl text-gray-400">
+            Here at Swift, we focus on markets where technology and capital can
+            unlock long-term value and drive ecosystem.
+          </p>
+        </div>
+
+        <div className="space-y-8 lg:grid lg:grid-cols-3 sm:gap-6">
+          {/* Starter Plan */}
+          <div className="flex flex-col p-6 mx-auto max-w-lg text-center">
+            <h3 className="mb-4 text-2xl font-semibold">Starter</h3>
+            <p className="font-light sm:text-lg text-gray-400">
+              Best option for personal use & your next project.
+            </p>
+            <ul role="list" className="mb-8 space-y-4 text-left">
+              <li className="flex items-center space-x-3">
+                <svg
+                  className="flex-shrink-0 w-5 h-5 text-green-500"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L10 10.586l6.293-6.293z"
+                    clipRule="evenodd"
+                  />
+                </svg>
+                <span>Individual configuration</span>
+              </li>
+              <li className="flex items-center space-x-3">
+                <svg
+                  className="flex-shrink-0 w-5 h-5 text-green-500"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L10 10.586l6.293-6.293z"
+                    clipRule="evenodd"
+                  />
+                </svg>
+                <span>No setup, or hidden fees</span>
+              </li>
+              <li className="flex items-center space-x-3">
+                <svg
+                  className="flex-shrink-0 w-5 h-5 text-green-500"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L10 10.586l6.293-6.293z"
+                    clipRule="evenodd"
+                  />
+                </svg>
+                <span>
+                  Team size:{" "}
+                  <span className="font-semibold">1 developer</span>
+                </span>
+              </li>
+              <li className="flex items-center space-x-3">
+                <svg
+                  className="flex-shrink-0 w-5 h-5 text-green-500"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L10 10.586l6.293-6.293z"
+                    clipRule="evenodd"
+                  />
+                </svg>
+                <span>
+                  Premium support:{" "}
+                  <span className="font-semibold">6 months</span>
+                </span>
+              </li>
+              <li className="flex items-center space-x-3">
+                <svg
+                  className="flex-shrink-0 w-5 h-5 text-green-500"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L10 10.586l6.293-6.293z"
+                    clipRule="evenodd"
+                  />
+                </svg>
+                <span>
+                  Free updates:{" "}
+                  <span className="font-semibold">6 months</span>
+                </span>
+              </li>
+            </ul>
+            <a
+              href="#"
+              className="text-white hover:bg-[#1B57E9] bg-primary font-medium rounded-lg text-sm px-5 py-2.5"
+            >
+              Get started
+            </a>
+          </div>
+
+          {/* Company Plan */}
+          <div className="flex flex-col p-6 mx-auto max-w-lg text-center">
+            <h3 className="mb-4 text-2xl font-semibold">Company</h3>
+            <p className="font-light sm:text-lg text-gray-400">
+              Relevant for multiple users, extended & premium support.
+            </p>
+            <div className="flex justify-center items-baseline mb-4">
+              <span className="mr-2 text-5xl font-extrabold">$99</span>
+              <span className="text-gray-400">/month</span>
+            </div>
+            <ul role="list" className="mb-8 space-y-4 text-left">
+              <li className="flex items-center space-x-3">
+                <svg
+                  className="flex-shrink-0 w-5 h-5 text-green-500"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L10 10.586l6.293-6.293z"
+                    clipRule="evenodd"
+                  />
+                </svg>
+                <span>Individual configuration</span>
+              </li>
+              <li className="flex items-center space-x-3">
+                <svg
+                  className="flex-shrink-0 w-5 h-5 text-green-500"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L10 10.586l6.293-6.293z"
+                    clipRule="evenodd"
+                  />
+                </svg>
+                <span>No setup, or hidden fees</span>
+              </li>
+              <li className="flex items-center space-x-3">
+                <svg
+                  className="flex-shrink-0 w-5 h-5 text-green-500"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L10 10.586l6.293-6.293z"
+                    clipRule="evenodd"
+                  />
+                </svg>
+                <span>
+                  Team size:{" "}
+                  <span className="font-semibold">10 developers</span>
+                </span>
+              </li>
+              <li className="flex items-center space-x-3">
+                <svg
+                  className="flex-shrink-0 w-5 h-5 text-green-500"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L10 10.586l6.293-6.293z"
+                    clipRule="evenodd"
+                  />
+                </svg>
+                <span>
+                  Premium support:{" "}
+                  <span className="font-semibold">24 months</span>
+                </span>
+              </li>
+              <li className="flex items-center space-x-3">
+                <svg
+                  className="flex-shrink-0 w-5 h-5 text-green-500"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L10 10.586l6.293-6.293z"
+                    clipRule="evenodd"
+                  />
+                </svg>
+                <span>
+                  Free updates:{" "}
+                  <span className="font-semibold">24 months</span>
+                </span>
+              </li>
+            </ul>
+            <a
+              href="#"
+              className="text-white hover:bg-[#1B57E9] bg-primary font-medium rounded-lg text-sm px-5 py-2.5"
+            >
+              Get started
+            </a>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Payments;
